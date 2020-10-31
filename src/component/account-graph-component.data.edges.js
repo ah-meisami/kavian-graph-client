05263ccf0d5ebf923e16efd631a649d30f45f21e
Meisami Amir Hossein <Ah_Meisami@iranet.net>
2020-10-31 13:36:25 +0330

    first release of graph visulization using visjs"

---
+++ src/component/account-graph-component.data.edges.js
@@ -0,0 +1,612 @@
+export const edges = [
+  {
+    "id": 53352,
+    "from": 170395,
+    "to": 124506,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0201404504009",
+    "edge_attr_trn_cd": "ZC78",
+    "edge_attr_trn_date": 13970807,
+    "edge_attr_trn_amnt": 30000000
+  },
+  {
+    "id": 87179,
+    "from": 170395,
+    "to": 124506,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0201404504009",
+    "edge_attr_trn_cd": "ZC78",
+    "edge_attr_trn_date": 13970815,
+    "edge_attr_trn_amnt": 25000000
+  },
+  {
+    "id": 100328,
+    "from": 170395,
+    "to": 186052,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0202075951001",
+    "edge_attr_trn_cd": "PM05",
+    "edge_attr_trn_date": 13970802,
+    "edge_attr_trn_amnt": 12000000
+  },
+  {
+    "id": 102994,
+    "from": 170395,
+    "to": 172583,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0201922880009",
+    "edge_attr_trn_cd": "PM05",
+    "edge_attr_trn_date": 13970815,
+    "edge_attr_trn_amnt": 30000000
+  },
+  {
+    "id": 104051,
+    "from": 170395,
+    "to": 302305,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0202967593006",
+    "edge_attr_trn_cd": "ZC78",
+    "edge_attr_trn_date": 13970815,
+    "edge_attr_trn_amnt": 7000000
+  },
+  {
+    "id": 117604,
+    "from": 170395,
+    "to": 186052,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0202075951001",
+    "edge_attr_trn_cd": "ZC78",
+    "edge_attr_trn_date": 13970917,
+    "edge_attr_trn_amnt": 30000000
+  },
+  {
+    "id": 137530,
+    "from": 170395,
+    "to": 354022,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0301086886000",
+    "edge_attr_trn_cd": "ZC78",
+    "edge_attr_trn_date": 13970823,
+    "edge_attr_trn_amnt": 100000000
+  },
+  {
+    "id": 171651,
+    "from": 170395,
+    "to": 273747,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0202726258003",
+    "edge_attr_trn_cd": "PM05",
+    "edge_attr_trn_date": 13970905,
+    "edge_attr_trn_amnt": 3000000
+  },
+  {
+    "id": 198141,
+    "from": 170395,
+    "to": 94671,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0201034277000",
+    "edge_attr_trn_cd": "PM05",
+    "edge_attr_trn_date": 13970826,
+    "edge_attr_trn_amnt": 250000
+  },
+  {
+    "id": 202250,
+    "from": 170395,
+    "to": 242318,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0202462570008",
+    "edge_attr_trn_cd": "ZC78",
+    "edge_attr_trn_date": 13970824,
+    "edge_attr_trn_amnt": 39000000
+  },
+  {
+    "id": 208592,
+    "from": 170395,
+    "to": 186052,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0202075951001",
+    "edge_attr_trn_cd": "ZC78",
+    "edge_attr_trn_date": 13970827,
+    "edge_attr_trn_amnt": 15000000
+  },
+  {
+    "id": 213063,
+    "from": 170395,
+    "to": 105491,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0201187871005",
+    "edge_attr_trn_cd": "PM05",
+    "edge_attr_trn_date": 13970910,
+    "edge_attr_trn_amnt": 2000000
+  },
+  {
+    "id": 218026,
+    "from": 170395,
+    "to": 300547,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0202952359002",
+    "edge_attr_trn_cd": "ZC78",
+    "edge_attr_trn_date": 13970826,
+    "edge_attr_trn_amnt": 48000000
+  },
+  {
+    "id": 224891,
+    "from": 170395,
+    "to": 186052,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0202075951001",
+    "edge_attr_trn_cd": "PM05",
+    "edge_attr_trn_date": 13970827,
+    "edge_attr_trn_amnt": 10000000
+  },
+  {
+    "id": 225148,
+    "from": 170395,
+    "to": 184849,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0202064911000",
+    "edge_attr_trn_cd": "PM05",
+    "edge_attr_trn_date": 13970909,
+    "edge_attr_trn_amnt": 2000000
+  },
+  {
+    "id": 226703,
+    "from": 170395,
+    "to": 135690,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0201533615008",
+    "edge_attr_trn_cd": "ZC78",
+    "edge_attr_trn_date": 13970824,
+    "edge_attr_trn_amnt": 25800000
+  },
+  {
+    "id": 232956,
+    "from": 170395,
+    "to": 186052,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0202075951001",
+    "edge_attr_trn_cd": "PM05",
+    "edge_attr_trn_date": 13970908,
+    "edge_attr_trn_amnt": 15000000
+  },
+  {
+    "id": 256563,
+    "from": 170395,
+    "to": 186052,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0202075951001",
+    "edge_attr_trn_cd": "PM05",
+    "edge_attr_trn_date": 13971001,
+    "edge_attr_trn_amnt": 2910000
+  },
+  {
+    "id": 259102,
+    "from": 170395,
+    "to": 186052,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0202075951001",
+    "edge_attr_trn_cd": "PM05",
+    "edge_attr_trn_date": 13970921,
+    "edge_attr_trn_amnt": 1010000
+  },
+  {
+    "id": 260404,
+    "from": 170395,
+    "to": 172583,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0201922880009",
+    "edge_attr_trn_cd": "PM05",
+    "edge_attr_trn_date": 13971003,
+    "edge_attr_trn_amnt": 5000000
+  },
+  {
+    "id": 265086,
+    "from": 170395,
+    "to": 186052,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0202075951001",
+    "edge_attr_trn_cd": "PM05",
+    "edge_attr_trn_date": 13971009,
+    "edge_attr_trn_amnt": 21610000
+  },
+  {
+    "id": 269195,
+    "from": 170395,
+    "to": 300547,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0202952359002",
+    "edge_attr_trn_cd": "ZC78",
+    "edge_attr_trn_date": 13970920,
+    "edge_attr_trn_amnt": 48000000
+  },
+  {
+    "id": 269744,
+    "from": 176501,
+    "to": 170395,
+    "node_from_acct": "0201969237005",
+    "node_to_acct": "0201894993002",
+    "edge_attr_trn_cd": "ZC78",
+    "edge_attr_trn_date": 13971012,
+    "edge_attr_trn_amnt": 92213470
+  },
+  {
+    "id": 270037,
+    "from": 170395,
+    "to": 170448,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0201895462006",
+    "edge_attr_trn_cd": "ZC78",
+    "edge_attr_trn_date": 13971018,
+    "edge_attr_trn_amnt": 192450000
+  },
+  {
+    "id": 276134,
+    "from": 170395,
+    "to": 273747,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0202726258003",
+    "edge_attr_trn_cd": "PM05",
+    "edge_attr_trn_date": 13971012,
+    "edge_attr_trn_amnt": 5000000
+  },
+  {
+    "id": 289725,
+    "from": 170395,
+    "to": 124506,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0201404504009",
+    "edge_attr_trn_cd": "ZC78",
+    "edge_attr_trn_date": 13971017,
+    "edge_attr_trn_amnt": 30000000
+  },
+  {
+    "id": 295699,
+    "from": 293978,
+    "to": 170395,
+    "node_from_acct": "0202895534008",
+    "node_to_acct": "0201894993002",
+    "edge_attr_trn_cd": "DC78",
+    "edge_attr_trn_date": 13971009,
+    "edge_attr_trn_amnt": 350000000
+  },
+  {
+    "id": 301047,
+    "from": 170395,
+    "to": 273747,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0202726258003",
+    "edge_attr_trn_cd": "PM05",
+    "edge_attr_trn_date": 13970927,
+    "edge_attr_trn_amnt": 436000
+  },
+  {
+    "id": 317789,
+    "from": 170395,
+    "to": 124506,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0201404504009",
+    "edge_attr_trn_cd": "ZC78",
+    "edge_attr_trn_date": 13970920,
+    "edge_attr_trn_amnt": 19000000
+  },
+  {
+    "id": 318244,
+    "from": 170395,
+    "to": 186052,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0202075951001",
+    "edge_attr_trn_cd": "PM05",
+    "edge_attr_trn_date": 13971001,
+    "edge_attr_trn_amnt": 6500000
+  },
+  {
+    "id": 320939,
+    "from": 170395,
+    "to": 186052,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0202075951001",
+    "edge_attr_trn_cd": "ZC78",
+    "edge_attr_trn_date": 13971005,
+    "edge_attr_trn_amnt": 7000000
+  },
+  {
+    "id": 325776,
+    "from": 170395,
+    "to": 169755,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0201887539007",
+    "edge_attr_trn_cd": "ZC78",
+    "edge_attr_trn_date": 13971019,
+    "edge_attr_trn_amnt": 97010000
+  },
+  {
+    "id": 326194,
+    "from": 170395,
+    "to": 284040,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0202816135005",
+    "edge_attr_trn_cd": "ZC78",
+    "edge_attr_trn_date": 13971011,
+    "edge_attr_trn_amnt": 26600000
+  },
+  {
+    "id": 328556,
+    "from": 170395,
+    "to": 94671,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0201034277000",
+    "edge_attr_trn_cd": "PM05",
+    "edge_attr_trn_date": 13970922,
+    "edge_attr_trn_amnt": 3400000
+  },
+  {
+    "id": 343176,
+    "from": 170395,
+    "to": 94671,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0201034277000",
+    "edge_attr_trn_cd": "PM05",
+    "edge_attr_trn_date": 13971017,
+    "edge_attr_trn_amnt": 3000000
+  },
+  {
+    "id": 343748,
+    "from": 172583,
+    "to": 170395,
+    "node_from_acct": "0201922880009",
+    "node_to_acct": "0201894993002",
+    "edge_attr_trn_cd": "PM05",
+    "edge_attr_trn_date": 13971005,
+    "edge_attr_trn_amnt": 25000000
+  },
+  {
+    "id": 344484,
+    "from": 170395,
+    "to": 197024,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0202145867003",
+    "edge_attr_trn_cd": "ZC78",
+    "edge_attr_trn_date": 13971008,
+    "edge_attr_trn_amnt": 30000000
+  },
+  {
+    "id": 365943,
+    "from": 170395,
+    "to": 362638,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0301253647000",
+    "edge_attr_trn_cd": "PM05",
+    "edge_attr_trn_date": 13970924,
+    "edge_attr_trn_amnt": 2000000
+  },
+  {
+    "id": 366641,
+    "from": 170395,
+    "to": 250777,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0202537307004",
+    "edge_attr_trn_cd": "ZC78",
+    "edge_attr_trn_date": 13971012,
+    "edge_attr_trn_amnt": 80000000
+  },
+  {
+    "id": 397521,
+    "from": 267906,
+    "to": 170395,
+    "node_from_acct": "0202675028001",
+    "node_to_acct": "0201894993002",
+    "edge_attr_trn_cd": "DC78",
+    "edge_attr_trn_date": 13971109,
+    "edge_attr_trn_amnt": 400000000
+  },
+  {
+    "id": 399734,
+    "from": 170395,
+    "to": 147954,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0201654850009",
+    "edge_attr_trn_cd": "ZC78",
+    "edge_attr_trn_date": 13971030,
+    "edge_attr_trn_amnt": 27000000
+  },
+  {
+    "id": 406363,
+    "from": 176501,
+    "to": 170395,
+    "node_from_acct": "0201969237005",
+    "node_to_acct": "0201894993002",
+    "edge_attr_trn_cd": "DC78",
+    "edge_attr_trn_date": 13971117,
+    "edge_attr_trn_amnt": 1500000000
+  },
+  {
+    "id": 406794,
+    "from": 170395,
+    "to": 300547,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0202952359002",
+    "edge_attr_trn_cd": "ZC78",
+    "edge_attr_trn_date": 13971118,
+    "edge_attr_trn_amnt": 25000000
+  },
+  {
+    "id": 410804,
+    "from": 170395,
+    "to": 124506,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0201404504009",
+    "edge_attr_trn_cd": "ZC78",
+    "edge_attr_trn_date": 13971118,
+    "edge_attr_trn_amnt": 42000000
+  },
+  {
+    "id": 411381,
+    "from": 170395,
+    "to": 186052,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0202075951001",
+    "edge_attr_trn_cd": "PM05",
+    "edge_attr_trn_date": 13971104,
+    "edge_attr_trn_amnt": 15000000
+  },
+  {
+    "id": 415486,
+    "from": 170395,
+    "to": 186052,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0202075951001",
+    "edge_attr_trn_cd": "ZC78",
+    "edge_attr_trn_date": 13971103,
+    "edge_attr_trn_amnt": 35700000
+  },
+  {
+    "id": 419214,
+    "from": 170395,
+    "to": 265767,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0202657961008",
+    "edge_attr_trn_cd": "ZC78",
+    "edge_attr_trn_date": 13971113,
+    "edge_attr_trn_amnt": 25000000
+  },
+  {
+    "id": 419267,
+    "from": 170395,
+    "to": 265767,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0202657961008",
+    "edge_attr_trn_cd": "ZC78",
+    "edge_attr_trn_date": 13971113,
+    "edge_attr_trn_amnt": 16800000
+  },
+  {
+    "id": 419376,
+    "from": 170395,
+    "to": 265767,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0202657961008",
+    "edge_attr_trn_cd": "ZC78",
+    "edge_attr_trn_date": 13971118,
+    "edge_attr_trn_amnt": 30300000
+  },
+  {
+    "id": 443220,
+    "from": 170395,
+    "to": 300547,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0202952359002",
+    "edge_attr_trn_cd": "ZC78",
+    "edge_attr_trn_date": 13971118,
+    "edge_attr_trn_amnt": 20000000
+  },
+  {
+    "id": 453248,
+    "from": 293978,
+    "to": 170395,
+    "node_from_acct": "0202895534008",
+    "node_to_acct": "0201894993002",
+    "edge_attr_trn_cd": "DC78",
+    "edge_attr_trn_date": 13971030,
+    "edge_attr_trn_amnt": 1600000000
+  },
+  {
+    "id": 456837,
+    "from": 170395,
+    "to": 242318,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0202462570008",
+    "edge_attr_trn_cd": "ZC78",
+    "edge_attr_trn_date": 13971107,
+    "edge_attr_trn_amnt": 30000000
+  },
+  {
+    "id": 456838,
+    "from": 170395,
+    "to": 242318,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0202462570008",
+    "edge_attr_trn_cd": "ZC78",
+    "edge_attr_trn_date": 13971107,
+    "edge_attr_trn_amnt": 72500000
+  },
+  {
+    "id": 463482,
+    "from": 170395,
+    "to": 178463,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0201991535005",
+    "edge_attr_trn_cd": "ZC78",
+    "edge_attr_trn_date": 13971116,
+    "edge_attr_trn_amnt": 5000000
+  },
+  {
+    "id": 470343,
+    "from": 170395,
+    "to": 186052,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0202075951001",
+    "edge_attr_trn_cd": "ZC78",
+    "edge_attr_trn_date": 13971110,
+    "edge_attr_trn_amnt": 150000000
+  },
+  {
+    "id": 471130,
+    "from": 170395,
+    "to": 124506,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0201404504009",
+    "edge_attr_trn_cd": "ZC78",
+    "edge_attr_trn_date": 13971109,
+    "edge_attr_trn_amnt": 30000000
+  },
+  {
+    "id": 471560,
+    "from": 170395,
+    "to": 186052,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0202075951001",
+    "edge_attr_trn_cd": "ZC78",
+    "edge_attr_trn_date": 13971106,
+    "edge_attr_trn_amnt": 28000000
+  },
+  {
+    "id": 472096,
+    "from": 170395,
+    "to": 164857,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0201831499009",
+    "edge_attr_trn_cd": "ZC78",
+    "edge_attr_trn_date": 13971106,
+    "edge_attr_trn_amnt": 24000000
+  },
+  {
+    "id": 492996,
+    "from": 267906,
+    "to": 170395,
+    "node_from_acct": "0202675028001",
+    "node_to_acct": "0201894993002",
+    "edge_attr_trn_cd": "DC78",
+    "edge_attr_trn_date": 13971106,
+    "edge_attr_trn_amnt": 634320418
+  },
+  {
+    "id": 495564,
+    "from": 170395,
+    "to": 335106,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0203230049008",
+    "edge_attr_trn_cd": "ZC78",
+    "edge_attr_trn_date": 13971023,
+    "edge_attr_trn_amnt": 100200000
+  },
+  {
+    "id": 514571,
+    "from": 170395,
+    "to": 186052,
+    "node_from_acct": "0201894993002",
+    "node_to_acct": "0202075951001",
+    "edge_attr_trn_cd": "PM05",
+    "edge_attr_trn_date": 13971115,
+    "edge_attr_trn_amnt": 7400000
+  }
+];
--- src/component/account-graph-component.js
+++ src/component/account-graph-component.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
-import React, { Component } from 'react';
+import React, { Component, createRef } from "react";
 import './account-graph-component.css';
+import { DataSet, Network } from 'vis';
+
+// import { nodes_offline } from './account-graph-component.data.nodes' // for offline testing
+// import { edges_offline } from './account-graph-component.data.nodes' // for offline testing
 
 export default class AccountGraphComponent extends Component {
 	constructor(props) {
@@ -10,6 +14,10 @@ export default class AccountGraphComponent extends Component {
       nodes: [],
       edges: []
     };
+
+
+    this.network = {};
+    this.appRef = createRef();
   }
 
   componentDidMount() {
@@ -23,19 +31,38 @@ export default class AccountGraphComponent extends Component {
       .then(response => {
         const nodes = response.data;
         this.setState({ nodes });
-      })
 
-    axios.get(url2)
-      .then(response => {
-        const edges = response.data;
-        this.setState({ edges });
-      })
+        axios.get(url2)
+          .then(response => {
+            const edges = response.data;
+            this.setState({ edges });
+
+            // for offline testing
+            // const vis_nodes = new DataSet(nodes_offline);
+            // const vis_edges = new DataSet(edges_offline);
+
+            // initialize network using API!
+            const vis_nodes = new DataSet(this.state.nodes);
+            const vis_edges = new DataSet(this.state.edges);
+
+            const vis_data = {
+              nodes: vis_nodes,
+              edges: vis_edges
+            };
+
+            const vis_options = {};
+
+            this.network = new Network(this.appRef.current, vis_data, vis_options);
+
+          })
+      });
   }
 
 	render() {
 		return (
       <div className="AccountGraphComponent">
-        {this.props.accNo}
+         {this.props.accNo}
+        <div ref={this.appRef} style={{ width: '100%', height: '400px', border: '1px solid black', align:'center'}} />
 			</div>
 		);
 	}
