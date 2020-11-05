efb01015c0ac33ac45f437aa558cb307c3ab9d84
Meisami Amir Hossein <Ah_Meisami@iranet.net>
2020-11-05 14:01:45 +0330

    complete account-option-component. allow user to change visjs online

--- src/components/account/account-option-data.js
+++ src/components/account/account-option-data.js
@@ -3,7 +3,10 @@ export const localOption ={
 		"enabled": false
 	},
 	"nodes": {
+		"color": "#6cafeb",
 		"font": {
+			"size": 16,
+			"color": "#000000",
 			"face": "Vazir"
 		},
 		"borderWidth": 2
